4a2ca07f7ccd2faf7b3c1029e375ec1b97ecf2a5
infoNitobe <9901148+infoNitobe@users.noreply.github.com>
2023-03-11 23:23:53 +0900

    write header to csv

--- src/xmlparser/parse_mindom.py
+++ src/xmlparser/parse_mindom.py
@@ -7,18 +7,24 @@ parent_path = str(parent_path).replace("\\", "/")
 input_path =  parent_path + "/" + "sample.xml"
 tree = minidom.parse(input_path)
 
-output_path = parent_path + "/" + "output.csv"
-f = open(output_path, "w")
-writer = csv.writer(f)
-writer.writerow("hello")
-
 elems = tree.getElementsByTagName("country")
+print(elems[0].childNodes)
+header = []
 for elem in elems:
+    if elem.nodeName not in header:
+        header.append(elem.nodeName)
     for node in list(elem.childNodes):
         if(node.nodeType == node.TEXT_NODE):
             pass
         else:
+            #for debug
             print(node)
+            if node.nodeName not in header:
+                header.append(node.nodeName)
+
+output_path = parent_path + "/" + "output.csv"
+with open(output_path, "w") as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
             
 tree.unlink()
-f.close()
