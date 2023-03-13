651695564dd924c564d61071522fa341c9d6b108
infoNitobe <9901148+infoNitobe@users.noreply.github.com>
2023-03-14 00:56:44 +0900

    conv xml to csv needed improvements

---
+++ src/xmlparser/xml2csv_text_node.py
@@ -0,0 +1,48 @@
+from xml.dom import minidom
+from pathlib import Path
+import csv
+
+parent_path = Path(__file__).resolve().parent
+parent_path = str(parent_path).replace("\\", "/")
+input_path =  parent_path + "/" + "sample_text_node.xml"
+tree = minidom.parse(input_path)
+
+elems = tree.getElementsByTagName("country")
+print(elems[0].childNodes)
+
+#make header
+header = []
+for elem in elems:
+    if elem.nodeName not in header and elem.nodeName != "country":
+        header.append(elem.nodeName)
+    for node in list(elem.childNodes):
+        #skip blank text node
+        if(node.nodeType == node.TEXT_NODE):
+            pass
+        else:
+            #for debug
+            #print(node)
+
+            if node.nodeName not in header:
+                header.append(node.nodeName)
+
+output_path = parent_path + "/" + "output.csv"
+f = open(output_path, "w")
+writer = csv.writer(f)
+writer.writerow(header)
+
+#make data
+for elem in elems:
+    text = []
+    #todo: why is this wrong?
+    #text.append(elem.getElementsByTagName("rank")[0].nodeValue)
+    #text.append(elem.getElementsByTagName("year")[0].nodeValue)
+
+    text.append(elem.getElementsByTagName("rank")[0].childNodes[0].nodeValue)
+    text.append(elem.getElementsByTagName("year")[0].childNodes[0].nodeValue)
+
+    writer.writerow(text)
+
+f.close()
+            
+tree.unlink()
